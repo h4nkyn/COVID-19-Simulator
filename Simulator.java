d46a2a8a73786a7b833c8ee0c70aa1df06a86681
Hank N <34946138+h4nkyn@users.noreply.github.com>
2021-03-15 00:29:07 -0400

    yay

--- Simulator.java
+++ Simulator.java
@@ -7,45 +7,49 @@ public class Simulator {
     //difficulty 1 = normal
     //difficulty 2 = hard
     //difficulty 3 = expert
-    int difficulty = 0;
-    String country = "United States";//["China","Iran", "Italy", "France", "Germany", "Spain", "Switzerland", "Turkey", "United Kingdom", "United States"];
-    int attitude = 50;
-    int attitudeChange = 10;
-    int availableICUs = 56000;
-    double progressTowardsVaccine = 0;
-    int populationTotal = 328460000;
-    int populationMinor = 60560000;
-    int populationAdult = 213830000;
-    int populationSenior = 54070000;
-    int infectedTotal = 0;
-    int infectedMinors = 0;
-    int infectedAdults = 0;
-    int infectedSeniors = 0;
-    int deadTotal = 0;
-    int deadMinors = 0;
-    int deadAdults = 0;
-    int deadSeniors = 0;
-    double minorMortality = 0;
-    double adultMortality = 0;
-    double seniorMortality = 0;
+    public static int difficulty = 0;
+    public static String country = "United States";//["China","Iran", "Italy", "France", "Germany", "Spain", "Switzerland", "Turkey", "United Kingdom", "United States"];
+    public static int attitude = 50;
+    public static int attitudeChange = 10;
+    public static int availableICUs = 56000;
+    public static double progressTowardsVaccine = 0;
+    public static int populationTotal = 328460000;
+    public static int populationMinor = 60560000;
+    public static int populationAdult = 213830000;
+    public static int populationSenior = 54070000;
+    public static int infectedTotal = 0;
+    public static int infectedMinors = 0;
+    public static int infectedAdults = 0;
+    public static int infectedSeniors = 0;
+    public static int deadTotal = 0;
+    public static int deadMinors = 0;
+    public static int deadAdults = 0;
+    public static int deadSeniors = 0;
+    public static double minorMortality = 0;
+    public static double adultMortality = 0;
+    public static double seniorMortality = 0;
     if(difficulty == 0){
       attitude = 80;
       minorMortality = 0.04;
-      adultMortality = 
+      adultMortality = 5.0
+      seniorMortality = 15.0;
     }
     if(difficulty == 1){
       attitude = 70;
       minorMortality = 0.04;
-      adultMortality = 
+      adultMortality = 6.5;
+      seniorMortality = 18.0;
     }
     if(difficulty == 2){
       attitude = 60;
       minorMortality = 0.10;
-      adultMortality = 
+      adultMortality = 10.0;
+      seniorMortality = 25.0;
     }
     else {
       minorMortality = 2.0;
-      adultMortality = 
+      adultMortality = 15.0;
+      seniorMortality = 32.0;
     }
   }
 }
